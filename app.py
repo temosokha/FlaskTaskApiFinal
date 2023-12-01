284a5e453e1c5419f061ab1ee9d5639756b858f1
Temo <temosokh@gmail.com>
2023-12-01 18:50:09 +0400

    delete unnecessary file

--- app.py
+++ app.py
@@ -250,6 +250,6 @@ SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
 swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': "Task Management API"})
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
-print("Flask App Name:", app.name)
+
 if __name__ == '__main__':
     app.run(debug=True)
